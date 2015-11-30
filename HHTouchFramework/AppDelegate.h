96a5b78ce0df75710a2e42182db89a6edb428d96
huihui cao <517566879@qq.com>
2015-11-30 17:48:22 +0800

    Initial Commit

---
+++ HHTouchFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHTouchFramework
+//
+//  Created by caohuihui on 15/11/30.
+//  Copyright © 2015年 caohuihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHTouchFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHTouchFramework
+//
+//  Created by caohuihui on 15/11/30.
+//  Copyright © 2015年 caohuihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
